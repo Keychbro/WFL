e473b3fc6b38bb11b502fce9683f235c47c40522
NiKNexT_T <niknextgo@gmail.com>
2024-04-12 23:48:24 +0300

    add diamond pass design

---
+++ Assets/Scripts/Game/DiamondPass/DiamondPassView.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace WOFL.DiamondPass
+{
+    public class DiamondPassView : MonoBehaviour
+    {
+        #region Variables
+
+        [Header("Objects")]
+        [SerializeField] private Image _background;
+        [SerializeField] private Image _ticket;
+        [SerializeField] private Image _icon;
+        [Space]
+        [SerializeField] private TextMeshProUGUI _textPrice;
+        [SerializeField] private Button _buyButton;
+        [Space]
+        [SerializeField] private Slider _levelsBar;
+        [SerializeField] private TextMeshProUGUI _stageText;
+        [SerializeField] private TextMeshProUGUI _progressText;
+
+        #endregion
+
+        #region Control Methods
+
+
+
+
+        #endregion
+    }
+}
